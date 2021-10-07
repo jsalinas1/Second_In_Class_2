da4a29397337ae03ee0058d251aecc3d629ad3c0
Jezreel Salinas <jsalinas@cpp.edu>
2021-10-06 20:25:00 -0700

    Implemented a function that accepts 2D array as parameter. Also initalized a 3 by 3 array

--- magicmain.c
+++ magicmain.c
@@ -1,6 +1,14 @@
-#include <stdlib.io>
+#include <stdio.h>
+#include <time.h>
+
+
+char is_Magic(int a[3][3], int n){
+    return 'Y';
+}
 
 int main(){
-
+    int n_size = 3;
+    int arr[n_size][n_size];
+    is_Magic(arr, n_size);
     return 0;
 }
